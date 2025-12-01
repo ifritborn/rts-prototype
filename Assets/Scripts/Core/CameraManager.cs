db4cc1cd3ea32342c58e297860447fd2bcda0a00
Ifritborn <ifritborndesigns@gmail.com>
2025-11-30 17:31:46 -0700

    camera scrolling based on mouse bounded to tilemap limits, added keyboard map movement, set up the start method to avoid recalculating variables every frame

--- Assets/Scripts/Core/CameraManager.cs
+++ Assets/Scripts/Core/CameraManager.cs
@@ -1,9 +1,22 @@
 
+using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 
 public class CameraManager : MonoBehaviour
 {
+
+
+    [SerializeField] private Tilemap tilemap;
+
+    [SerializeField] private  int mapPadding;
+
+    [SerializeField] private  int panThresholdPercent;
+
+    [SerializeField] private  float cameraSpeedMouse;
+
+    [SerializeField] private  float cameraSpeedKeyBoard;
 
     private enum Bumper
     {
@@ -13,91 +26,197 @@ public class CameraManager : MonoBehaviour
         Down
     }
 
+    // establishes left and right bumper for camera movement
+    private int RLThresh;
+    private float rBumper;
+    private float lBumper;
+
+    // establishes up and down bumper for camera movement
+    private int UDThresh;
+    private float topBumper;
+    private float botBumper;
+
+    // establishes how fast the camera moves
+    private float mtime;
+
+    // establish map bounds
+    private Bounds bounds;
+
+    // Get camera dimentions and position
+    private Camera cam;
+    private float aspect;
+    private float camHalfWidth;
+    private float camHalfHeight;
+
+    // Set min/max for x y camera movement
+    private float minX;
+    private float maxX;
+
+    private float minY;
+    private float maxY;
+
+    private float zoomMax = 10;
+    private float zoomMin = 1;
+
+    void Start()
+    {
+        // establishes left and right bumper for camera movement
+        RLThresh = Screen.width / panThresholdPercent;
+        rBumper = Screen.width - RLThresh;
+        lBumper = RLThresh;
+
+        // establishes up and down bumper for camera movement
+        UDThresh = Screen.height / panThresholdPercent;
+        topBumper = Screen.height - UDThresh;
+        botBumper = UDThresh;
+
+
+
+        // Establish map bounds
+        bounds = tilemap.localBounds;
+
+        // Get camera dimentions and position
+        cam = Camera.main;
+        aspect = cam.aspect;
+        camHalfWidth = cam.orthographicSize * aspect;
+        camHalfHeight = cam.orthographicSize;
+
+        // Set min/max for x y camera movement
+        minX = bounds.min.x + camHalfWidth - mapPadding;
+        maxX = bounds.max.x - camHalfWidth + mapPadding;
+        minY = bounds.min.y + camHalfHeight - mapPadding;
+        maxY = bounds.max.y - camHalfHeight + mapPadding;
+    }
+
+    private void moveMapWithKeyboard()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector3 moveDelta = Vector3.right * cameraSpeedKeyBoard * mtime;
+            Vector3 newPos = transform.position + moveDelta;
+            float rBoundry = Mathf.Clamp(newPos.x, minX, maxX);
+            transform.position = new Vector3(rBoundry, newPos.y, newPos.z);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector3 moveDelta = Vector3.left * cameraSpeedKeyBoard * mtime;
+            Vector3 newPos = transform.position + moveDelta;
+            float rBoundry = Mathf.Clamp(newPos.x, minX, maxX);
+            transform.position = new Vector3(rBoundry, newPos.y, newPos.z);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 moveDelta = Vector3.up * cameraSpeedKeyBoard * mtime;
+            Vector3 newPos = transform.position + moveDelta;
+            float dBoundary = Mathf.Clamp(newPos.y, minY, maxY);
+            transform.position = new Vector3(newPos.x, dBoundary, newPos.z);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 moveDelta = Vector3.down * cameraSpeedKeyBoard * mtime;
+            Vector3 newPos = transform.position + moveDelta;
+            float dBoundary = Mathf.Clamp(newPos.y, minY, maxY);
+            transform.position = new Vector3(newPos.x, dBoundary, newPos.z);
+        }
+    }
+
     private void moveMapWithMouse()
     {
         Vector3 mpos = Input.mousePosition;
+        Vector3 camPos = cam.transform.position;
 
-        int RLThresh = Screen.width / 10; //192
-        float rBumper = Screen.width - RLThresh; //1728
-        float lBumper = RLThresh;
+        float currentZoom = cam.orthographicSize;
+        float scrollDelta = Input.mouseScrollDelta.y * mtime *100;
 
-        int UDThresh = Screen.height / 10;
-        float topBumper = Screen.height - UDThresh;
-        float botBumper = UDThresh;
+        if (scrollDelta != 0)
+        {
+            //TODO: map bounds need to be adjusted on zoom otherwise when zoomed in cant scroll all the way to edge of map
+            float newZoom = currentZoom + scrollDelta;
+            float zoomBoundary = Mathf.Clamp(newZoom, zoomMin, zoomMax);
+            cam.orthographicSize = zoomBoundary;
+            
+        }
 
-        float mtime = Time.deltaTime;
-        float cameraSpeed = 5f;
-        
-        // float mCalc = mtime * cameraSpeed * speedMult;
 
         // Move camera to the Right
-        if ( mpos.x <= Screen.width && mpos.x >= rBumper)
+        if (mpos.x <= Screen.width && mpos.x >= rBumper)
         {
-            Vector3 moveRight = Vector3.right;
-            float sMult = speedMult(mpos, Bumper.Right, rBumper, RLThresh);
-            float mCalc = mtime * cameraSpeed * sMult;
-            transform.position += (moveRight * mCalc);
+            float mvSpdCalc = speedMult(mpos, Bumper.Right, rBumper, RLThresh);
+            Vector3 moveDelta = Vector3.right * mvSpdCalc;
+            Vector3 newPos = transform.position + moveDelta;
+            float rBoundry = Mathf.Clamp(newPos.x, minX, maxX);
+            transform.position = new Vector3(rBoundry, newPos.y, newPos.z);
         }
 
         // Move camera to the Left
         if (mpos.x >= 0 && mpos.x <= lBumper)
         {
-            Vector3 moveLeft = Vector3.left;
-            float sMult = speedMult(mpos, Bumper.Left, lBumper, RLThresh);
-            float mCalc = mtime * cameraSpeed * sMult;
-            transform.position += (moveLeft * mCalc);
+            float mvSpdCalc = speedMult(mpos, Bumper.Left, lBumper, RLThresh);
+            Vector3 moveDelta = Vector3.left * mvSpdCalc;
+            Vector3 newPos = transform.position + moveDelta;
+            float lBoundry = Mathf.Clamp(newPos.x, minX, maxX);
+            transform.position = new Vector3(lBoundry, newPos.y, newPos.z);
         }
 
         // Move camera up
         if (mpos.y <= Screen.height && mpos.y >= topBumper)
         {
-            Vector3 moveUp = Vector3.up;
-            float sMult = speedMult(mpos, Bumper.Up, topBumper, UDThresh);
-            float mCalc = mtime * cameraSpeed * sMult;
-            transform.position += (moveUp * mCalc);
+            float mvSpdCalc = speedMult(mpos, Bumper.Up, topBumper, UDThresh);
+            Vector3 moveDelta = Vector3.up * mvSpdCalc;
+            Vector3 newPos = transform.position + moveDelta;
+            float uBoundary = Mathf.Clamp(newPos.y, minY, maxY);
+            transform.position = new Vector3(newPos.x, uBoundary, newPos.z);
         }
 
         // Move camera down
         if (mpos.y >= 0 && mpos.y <= botBumper)
         {
-            Vector3 moveDown = Vector3.down;
-            float sMult = speedMult(mpos, Bumper.Down, botBumper, UDThresh);
-            float mCalc = mtime * cameraSpeed * sMult;
-            transform.position += (moveDown * mCalc);
+            float mvSpdCalc = speedMult(mpos, Bumper.Down, botBumper, UDThresh);
+            Vector3 moveDelta = Vector3.down * mvSpdCalc;
+            Vector3 newPos = transform.position + moveDelta;
+            float dBoundary = Mathf.Clamp(newPos.y, minY, maxY);
+            transform.position = new Vector3(newPos.x, dBoundary, newPos.z);
         }
     }
 
+    // this is the linear gradient that makes the camera go faster or slower while in the bumper zone
     private float speedMult(Vector3 mpos, Bumper b, float bumper, float thresh)
     {
+        float ratio = 0f;
+
         switch (b)
         {
             case Bumper.Right:
-                return Mathf.Clamp(((mpos.x - bumper) / thresh), 0f, 1f);
+                ratio = (mpos.x - bumper) / thresh;
+                break;
             case Bumper.Left:
-                return Mathf.Clamp(1 - (mpos.x / thresh), 0f, 1f);
+                ratio = 1 - (mpos.x / thresh);
+                break;
             case Bumper.Up:
-                return Mathf.Clamp(((mpos.y - bumper) / thresh), 0f, 1f);
+                ratio = (mpos.y - bumper) / thresh;
+                break;
             case Bumper.Down:
-                return Mathf.Clamp(1 - (mpos.y / thresh), 0f, 1f);;
-            default:
-                return 0f;
+                ratio = 1 - (mpos.y / thresh);
+                break;
         }
-  
+
+        float spdMulti = Mathf.Clamp(ratio, 0f, 1f);
+        float mvSpdCalc = mtime * cameraSpeedMouse * spdMulti;
+        return mvSpdCalc;
     }
 
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        // establishes how fast the camera moves
+        mtime = Time.deltaTime;
 
-        moveMapWithMouse();        
-        
+        moveMapWithMouse();
+        moveMapWithKeyboard();
+
     }
 }
