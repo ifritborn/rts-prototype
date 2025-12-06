1fabab2c2c8862969fa38cffe1d00806ba30e84b
Ifritborn <ifritborndesigns@gmail.com>
2025-12-06 13:38:03 -0700

    still setting up the basics

---
+++ Assets/Scripts/Buildings/Building.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+
+    [SerializeField] private bool isBase;
+    [SerializeField] private int maxHp;
+    private int currentHealth;
+
+    private void deathHandler()
+    {
+        if (!isBase)
+        {
+            // emit event to player to remove building from pool
+            // destroy self
+        }
+        else
+        {
+            // emit base destroyed event to game manager
+        }
+    }
+    public void takeDamage(int dmgVal)
+    {
+        
+        int updatedHp = currentHealth - dmgVal;
+        int hpBounds = Mathf.Clamp(updatedHp, 0, maxHp);
+        currentHealth = hpBounds;
+
+        if (currentHealth == 0)
+        {
+            deathHandler();
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        
+
+    }
+
+    void Start()
+    {
+        currentHealth = maxHp;
+    }
+
+}
